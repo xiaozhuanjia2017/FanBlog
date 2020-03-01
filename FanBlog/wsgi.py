2ac18695272f2c643332258bc5a9138bd8bf4313
Lily <johndoe@example.com>
2020-03-01 22:27:52 +0800

    修改 MyBlog名为 FanBlog

--- FanBlog/wsgi.py
+++ FanBlog/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for MyBlog project.
+WSGI config for FanBlog project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyBlog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FanBlog.settings')
 
 application = get_wsgi_application()
